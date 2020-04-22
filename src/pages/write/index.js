fb9993c3bcc9ee72291f13e6f4cf7860c19b9987
xiaofeng <804875728@qq.com>
2020-04-22 12:44:36 +0800

    文章内容页修改 && npm build

--- src/pages/write/index.js
+++ src/pages/write/index.js
@@ -1,22 +1,33 @@
-import React, { PureComponent } from 'react';
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { PureComponent } from "react";
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { Input, Button } from "antd";
+import { WriteMain, TextStyle } from "./style";
 
 class Write extends PureComponent {
-	render() {
-		const { loginStatus } = this.props;
-		if (loginStatus) {
-			return (
-				<div>写文章页面</div>
-			)
-		}else {
-			return <Redirect to='/login'/>
-		}
-	}
+  render() {
+    const { loginStatus } = this.props;
+    const { TextArea } = Input;
+    if (loginStatus) {
+      return (
+        <WriteMain>
+          <div>
+            <Input placeholder="文章标题" style={{ width: 400, height: 25 }} />
+          </div>
+          <TextStyle>
+            <TextArea rows={8} style={{ width: 400 }} placeholder="文章内容" />
+          </TextStyle>
+          <Button type="primary">提交</Button>
+        </WriteMain>
+      );
+    } else {
+      return <Redirect to="/login/entry" />;
+    }
+  }
 }
 
 const mapState = (state) => ({
-	loginStatus: state.getIn(['login', 'login'])
-})
+  loginStatus: state.getIn(["login", "login"]),
+});
 
 export default connect(mapState, null)(Write);
---
+++ src/pages/write/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const WriteMain = styled.div`
+  margin-left: 33%;
+  margin-top: 5%;
+`;
+
+export const TextStyle = styled.div`
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
