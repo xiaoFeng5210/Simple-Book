fb7e1fc498d1b4c13d3755c7ecc6dbed43d4c893
xiaofeng <804875728@qq.com>
2020-04-22 15:24:39 +0800

    重新打包

--- src/pages/home/components/Writer.js
+++ src/pages/home/components/Writer.js
@@ -1,13 +1,10 @@
-import React, { PureComponent } from 'react';
-import { WriterWrapper } from '../style';
+import React, { PureComponent } from "react";
+import { WriterWrapper } from "../style";
 
 class Writer extends PureComponent {
-
-	render() {
-		return (
-			<WriterWrapper>HomeWork</WriterWrapper>
-		)
-	}
+  render() {
+    return <WriterWrapper>欢迎来到庆风的简书平台</WriterWrapper>;
+  }
 }
 
 export default Writer;
